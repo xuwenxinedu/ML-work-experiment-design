38b41b294a6275c6852156d3d8427171d2d0e8f0
xuwenxinedu <xuwenxin_edu@163.com>
2022-05-18 13:00:07 +0800

    2022年5月18日12点59分

--- work/DecesionTree/tree.py
+++ work/DecesionTree/tree.py
@@ -1,47 +1,28 @@
+from util import *
 import pandas as pd
 import numpy as np
 import easydict
 
 config = easydict.EasyDict({
-    "fileName": "dis.csv"
+    "fileName": "disease.csv"
 })
 
-class Util:
-    def __init__(self, data, label_name) -> None:
-        self.data = data
-        self.label_name = label_name
+class Tree:
+    def __init__(self, label_name) -> None:
+        self.label = label_name
     
-    staticmethod
-    def infor(data) -> float:
-        a = pd.value_counts(data) / len(data)
-        return sum(np.log2(a) * a * (-1))
-    
-    def col_infor(self, col) -> float:
-        col_dict = {}
-        for i, item in enumerate(self.data[col]):
-            if item in col_dict.keys():
-                col_dict[item].append(self.data[self.label_name][i])
-            else:
-                col_dict[item] = [self.data[self.label_name][i]]
-        ans = 0.0
-        for key in col_dict.keys():
-            ans += len(col_dict[key]) / len(self.data[col]) * Util.infor(col_dict[key])
-        return ans
+    def create_tree(self, data):
+        if self.all_same(data):
+            return data[self.label][0]
         
-        
-    
-    def max_info(self):
-        all_info = Util.infor(self.data[self.label_name])
-        maxn = 0
-        max_col = ''
-        for col in self.data.columns:
-            if col == self.label_name:
-                continue
-            col_info = self.col_infor(col)
-            sub = all_info - col_info
-            max_col = max_col if sub < maxn else col
-            maxn = maxn if sub < maxn else sub
-        return max_col
+
+    def all_same(self, data) ->bool:
+        s = set()
+        for item in data:
+            s.add(item)
+            if len(s) > 1:
+                return False
+        return True
 
 def main():
     df = pd.read_csv(config.fileName)
